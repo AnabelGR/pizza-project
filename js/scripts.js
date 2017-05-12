0a203730c51b215086cea97e1429a6911534e2ec
Anabel Ramirez <anabel@apparentinc.com>
2017-05-12 13:04:31 -0700

    change toggle to a script function on input.

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,25 @@ function showDiv() {
 
 //user interface logic
 $(document).ready(function(){
-  $("form.size").submit(function(event){
+  $("form#pizzaChoice").submit(function(event){
     event.preventDefault();
     $("input:checkbox[name=size]:checked").each(function(){
       var size = $(this).val();
-      $('.pizza-size').append(size);
-    });
-  });
-  $("form.style").submit(function(event){
-    event.preventDefault();
+      $('.pizza-size').append("<h3>Thank you for your order.</h3>" + "<h2>Your pizza:</h2>"
++ "<p>Size: " + size +"</p>");
+      });
     $("input:checkbox[name=style]:checked").each(function(){
       var style = $(this).val();
-      $('.pizza-style').append(style);
-    });
-  });
-
-  $("form.toppings").submit(function(event){
-    event.preventDefault();
+      $('.pizza-style').append("<p>Style: " + style +"</p>");
+      });
     $("input:checkbox[name=toppings]:checked").each(function(){
       var toppings = $(this).val();
-      $('.pizza-toppings').append(" " + toppings + " ");
+      $('.pizza-toppings').append("<p>Style: " + toppings + " " + "</p>");
     });
-        // $('#pizza').show;
+
+    $("#pizzaChoice").click(function() {
+      $("#confirmation").show();
+    });
   });
 
   $("form#delivery").submit(function(event) {
@@ -36,8 +33,7 @@ $(document).ready(function(){
     var inputtedName = $("input#name").val();
     var inputtedAddress = $("input#address").val();
     var inputtedPhone = $("input#phone").val();
-
-    $(".addressto").append(" " + inputtedName + " " + inputtedAddress + " " + inputtedPhone);
+    $(".addressto").append("<p>We are delivering to: " + inputtedName + " " + inputtedAddress + " " + inputtedPhone);
 
     $("#delivery-form").click(function() {
       $("#deliveryConfirm").show();
@@ -47,5 +43,4 @@ $(document).ready(function(){
     $("input#address").val("");
     $("input#phone").val("");
   });
-
 });
