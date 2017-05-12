e89cd8fac12a6624b4e43bc1ea955165adf55ce2
Anabel Ramirez <anabel@apparentinc.com>
2017-05-12 11:47:05 -0700

    add script to get input data from form.

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+//business logic
+// function for clearing document
+
+
+//user interface logic
+$(document).ready(function(){
+  $("form.size").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=size]:checked").each(function(){
+      var size = $(this).val();
+      $('.pizza-size').append(size);
+    });
+  });
+  $("form.style").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=style]:checked").each(function(){
+      var style = $(this).val();
+      $('.pizza-style').append(style);
+    });
+  });
+
+  $("form.toppings").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var toppings = $(this).val();
+      $('.pizza-toppings').append(" " + toppings + " ");
+    });
+        // $('#pizza').show;
+  });
+
+  $("form#delivery").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedName = $("input#name").val();
+    var inputtedAddress = $("input#address").val();
+    var inputtedPhone = $("input#phone").val();
+
+    $(".addressto").append(" " + inputtedName + " " + inputtedAddress + " " + inputtedPhone);
+
+    $("#delivery-form").click(function() {
+      $("#confirmation").show();
+    });
+
+    $("input#nname").val("");
+    $("input#address").val("");
+    $("input#phone").val("");
+  });
+});
